916a3fcc3354c0bafc57f2e5cf70ab03d6a34650
mayurk224 <mayurkamble0250@gmail.com>
2026-02-28 08:37:15 +0530

    feat: Add email sending service and database connection utility.

--- config/database.js
+++ config/database.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
 const uri = process.env.MONGO_URI;
 
 function connectToDb() {
-    mongoose.connect(uri).then(() => {
-        console.log("Connected to DB");
-    }).catch((err) => {
-        console.log("Error connecting to DB:", err);
+  mongoose
+    .connect(uri)
+    .then(() => {
+      console.log("Connected to DB");
     })
+    .catch((err) => {
+      console.log("Error connecting to DB:", err);
+    });
 }
 
 module.exports = connectToDb;
--- services/email.service.js
+++ services/email.service.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // Use true for port 465, false for port 587
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
